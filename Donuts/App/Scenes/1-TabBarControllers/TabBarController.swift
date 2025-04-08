94eda7b2f3dfdb43a16d6fe8ffbd11ee44e1cd0b
Diggo Silva <rdiggosilva@gmail.com>
2025-04-08 09:46:38 -0300

    Created TabBar

--- Donuts/App/Scenes/1-TabBarControllers/TabBarController.swift
+++ Donuts/App/Scenes/1-TabBarControllers/TabBarController.swift
@@ -5,4 +5,30 @@
 //  Created by Diggo Silva on 08/04/25.
 //
 
-import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+        navigationItem.hidesBackButton = true
+    }
+    
+    private func setupTabBar() {
+        UITabBar.appearance().isTranslucent = false
+        viewControllers = [createFeedNavController(), createFavoritesNavController()]
+    }
+    
+    private func createFeedNavController() -> UINavigationController {
+        let feedNacController = UINavigationController(rootViewController: FeedViewController())
+        feedNacController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        return feedNacController
+    }
+    
+    private func createFavoritesNavController() -> UINavigationController {
+        let favoritesNavController = UINavigationController(rootViewController: FeedViewController()) // CRIAR FAVORITES
+        favoritesNavController.tabBarItem = UITabBarItem(title: "Favoritos", image: UIImage(systemName: "star"), selectedImage: UIImage(systemName: "star.fill"))
+        return favoritesNavController
+    }
+}
